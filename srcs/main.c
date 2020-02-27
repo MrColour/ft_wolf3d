8bbef7afc4b78c56f0d7ec4ef130cfe7b9337aee
Kevin Mira <kmira@e1z2r5p16.42.us.org>
2020-02-27 00:43:06 -0800

    Set window stuff ready

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kmira <kmira@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 14:31:31 by kmira             #+#    #+#             */
-/*   Updated: 2020/02/27 00:35:20 by kmira            ###   ########.fr       */
+/*   Updated: 2020/02/27 00:43:05 by kmira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,10 @@ void	draw_circle(void)
 	j = 0;
 	i = 0;
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3ub(250, 0, 0);
 	glBegin(GL_POINTS);
 	glColor3ub(250, 0, 0);
 	while (i < (M_PI / 2))
 	{
-		if (j % 2 == 0)
-			glColor3ub(0, 0, 250);
-		else
-			glColor3ub(0, 250, 0);
 		x = 256 * cos(i);
 		y = 256 * sin(i);
 		glVertex2i(x, y);
